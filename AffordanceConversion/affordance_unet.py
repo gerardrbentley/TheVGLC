4b6066954fa8edc042ecc827565fbdb93da307dd
gbkh2015 <gbkh2015@mymail.pomona.edu>
2020-03-04 13:15:28 -0800

    mm and ki affordances, model prediction start

---
+++ AffordanceConversion/affordance_unet.py
@@ -0,0 +1,198 @@
+"""
+Based on template from https://github.com/PyTorchLightning/pytorch-lightning/blob/master/pl_examples/basic_examples/lightning_module_template.py
+
+Adapted by Gerard
+"""
+import logging as log
+import os
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torchvision.datasets import MNIST
+
+import pytorch_lightning as pl
+
+# import mlflow
+# import mlflow.pytorch
+
+import input_target_transforms as TT
+import distributed_utils
+
+# from faim_mlflow import get_run_manager
+from ml_args import parse_args
+from models import get_model, UNetAuto
+
+from datasets import GameImagesDataset, GameFoldersDataset, OverfitDataset, get_dataset
+
+
+class AffordanceUnetLightning(pl.LightningModule):
+    """
+    Unet Auto Encoder models with pytorch lightning
+    """
+
+    def __init__(self, hparams):
+        """
+        Pass in parsed HyperOptArgumentParser to the model
+        :param hparams:
+        """
+        # init superclass
+        super(AffordanceUnetLightning, self).__init__()
+        self.hparams = hparams
+
+        self.batch_size = hparams.batch_size
+
+        self.dataset = None
+        # build model
+        self.net = UNetAuto(num_out_channels=10, max_features=1024)
+    # ---------------------
+    # TRAINING
+    # ---------------------
+
+    def forward(self, x):
+        """
+        Based on model choice
+        :param x:
+        :return:
+        """
+        return self.net.forward(x)
+
+    def loss(self, targets, segmentations):
+        """
+        Combines sigmoid with Binary Cross Entropy for numerical stability.
+        Can include pos_weight to increase recall of underrepresented classes
+        :param targets: ground truth image or segmentation map
+        :param segmentations: output of the autoencoder network
+        """
+        mse = F.binary_cross_entropy_with_logits(segmentations, targets)
+        return mse
+
+    def training_step(self, batch, batch_idx):
+        """
+        Lightning calls this inside the training loop
+        :param batch:
+        :return:
+        """
+        # forward pass
+        images, targets = batch
+        predictions = self.forward(images)
+        # calculate loss
+        loss_val = self.loss(targets, predictions)
+
+        output = {
+            'loss': loss_val,  # required
+            'progress_bar': {'training_loss': loss_val},
+        }
+        return output
+
+    def validation_step(self, batch, batch_idx):
+        """
+        Called in validation loop with model in eval mode
+        """
+        images, targets = batch
+        predictions = self.forward(images)
+        loss_val = self.loss(targets, predictions)
+        return {'val_loss': loss_val}
+
+    def validation_end(self, outputs):
+        val_losses = torch.stack([x['val_loss'] for x in outputs])
+        avg_loss = val_losses.mean()
+        max_loss = val_losses.max()
+        return {'avg_val_loss': avg_loss,
+                'max_val_loss': max_loss}
+
+    # ---------------------
+    # TRAINING SETUP
+    # ---------------------
+    def configure_optimizers(self):
+        """
+        REQUIRED
+        can return multiple optimizers and learning_rate schedulers
+        Use lists to return lr scheduler, else can just return optimizer
+        :return: list of optimizers
+        """
+        args = self.hparams
+        optimizer = torch.optim.SGD(
+            self.net.parameters(),
+            lr=args.lr,
+            momentum=args.momentum, weight_decay=args.weight_decay
+        )
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+            factor=0.1,)
+        return [optimizer], [scheduler]
+
+    def load_full_dataset(self):
+        if self.dataset is None:
+            train_transform = (TT.get_transform(
+                train=True, inpaint=False, noise=False))
+            val_transform = (TT.get_transform(
+                train=False, inpaint=False, noise=False))
+            self.dataset = get_dataset(
+                self.hparams.dataset, 'train', transform=train_transform)
+
+            self.val_dataset = get_dataset(
+                self.hparams.dataset, 'val', transform=val_transform)
+
+            num_samples = len(self.dataset)
+            num_train = int(0.8 * num_samples)
+            indices = torch.randperm(num_samples).tolist()
+            self.dataset = torch.utils.data.Subset(
+                self.dataset, indices[0:num_train])
+            self.val_dataset = torch.utils.data.Subset(
+                self.val_dataset, indices[num_train:])
+            log.info(f'Full Dataset loaded: len: {num_samples}')
+            # self.train_split, self.val_split = torch.utils.data.random_split(
+            #     self.dataset, (num_train, num_samples - num_train))
+            log.info(
+                f'Train {len(self.dataset)} and Val {len(self.val_dataset)} splits made')
+
+            # print(self.train_split.transform)
+            # print(self.val_split.transform)
+            # x, y = self.train_split[0]
+            # print(type(x), type(y))
+            # z, p = self.train_split.transform(x, y)
+            # print(type(z), type(p))
+
+    @pl.data_loader
+    def train_dataloader(self):
+        """
+        Required
+        """
+        log.info('Training data loader called.')
+        self.load_full_dataset()
+        return torch.utils.data.DataLoader(self.dataset, batch_size=self.hparams.batch_size, drop_last=True)
+
+    @pl.data_loader
+    def val_dataloader(self):
+        log.info('Validation data loader called.')
+        self.load_full_dataset()
+        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.hparams.batch_size, drop_last=False)
+
+    # @pl.data_loader
+    # def test_dataloader(self):
+    #     log.info('Test data loader called.')
+    #     return self.__dataloader(train=False)
+
+
+def main(args):
+    # init module
+    model = AffordanceUnetLightning(args)
+
+    # most basic trainer, uses good defaults
+    trainer = pl.Trainer(
+        max_nb_epochs=args.epochs,
+        gpus=None if args.gpus == '' else args.gpus,
+        num_nodes=args.world_size,
+    )
+    trainer.fit(model)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
--- AffordanceConversion/txt_to_affordances.py
+++ AffordanceConversion/txt_to_affordances.py
@@ -25,21 +25,25 @@ def main(args):
         file_paths = glob.glob(os.path.join(args.folder, '*.txt'))
     else:
         file_paths = [args.file]
+    try:
+        with open(args.json) as f:
+            affordance_dict = json.load(f)
+            affordance_dict = affordance_dict['tiles']
+    except:
+        print('Error loading affordance json')
+        return
 
     grid_size = args.grid_size
-    for file_name in file_paths:
+    for file_path in file_paths:
         if args.verbose:
-            print(f'file: {file_name}')
+            print(f'file: {file_path}')
             print(args)
             print('getting level rows and cols from file')
-        file_name = os.path.split(file_name)[1]
+        file_name = os.path.split(file_path)[1]
         basename = os.path.splitext(file_name)[0]
         
         try:
-            with open(args.json) as f:
-                affordance_dict = json.load(f)
-                affordance_dict = affordance_dict['tiles']
-            with open(args.file) as processed_level:
+            with open(file_path) as processed_level:
                 level_rows = [[char for char in line if char != '\n'] for line in processed_level]
         except:
             print("Error loading affordance file or level map")
@@ -50,15 +54,12 @@ def main(args):
             if num_cols != len(line):
                 print(f"Not all lines the same width: {num_cols}, {len(line)}")
                 return
-        if args.verbose:
-            print(f"Num rows: {num_rows}, num cols: {num_cols}")
-            print(json.dumps(affordance_dict))
 
         height = num_rows * grid_size
         width = num_cols * grid_size
         affordance_map = np.zeros((height, width, len(AFFORDANCES)), dtype=np.float)
 
-        
+        buckets = np.zeros(len(AFFORDANCES))
         seen_chars = []
         for (row, col) in [(r, c) for r in range(num_rows) for c in range(num_cols)]:
             row_idx, col_idx = row*grid_size, col*grid_size
@@ -66,46 +67,64 @@ def main(args):
             char_affords = affordance_dict.get(curr_char, {})
             for affordance, value in char_affords.items():
                 affordance_map[row_idx:row_idx+grid_size, col_idx:col_idx+grid_size, AFFORDANCES.index(affordance)] = value
-
+                buckets[AFFORDANCES.index(affordance)] += int(grid_size*grid_size)
             if curr_char not in seen_chars and args.verbose:
                 seen_chars.append(curr_char)
                 print(f"Char {curr_char} Affords: {char_affords}")
                 
         if args.verbose:
             for affordance in AFFORDANCES:
-                print(f"Unique vals in affordance map for {affordance}: {np.unique(affordance_map[:,:,AFFORDANCES.index(affordance)])}")
-        if args.visualize:
-            visualize(affordance_map)
+                print(f"Unique vals {affordance}: {np.unique(affordance_map[:,:,AFFORDANCES.index(affordance)])}")
+            print(f"Num pixels per affordance: {buckets.tolist()}")
+            print(AFFORDANCES)
+            print(f"Total pixels {num_rows} rows, {num_cols} cols: (h*w) ({height} * {width}): {height*width}")
         if not args.no_save:
+            os.makedirs(args.dest, exist_ok=True)
             dest_file = os.path.join(args.dest, f"{basename}.npy")
             print(f'saving to {dest_file}')
             np.save(dest_file, affordance_map)
+    if args.visualize:
+        print('visualizing last affordance map solid and permeable (--visualizeall for all affordances)')
+        visualize(affordance_map, basename, only_solid_permeable=True)
 
-def visualize(affordance_map):
+def visualize(affordance_map, filename, only_solid_permeable=False):
     cmap = ListedColormap(['black', 'gray', 'white'])
     # Map 0 to black, 0.5 to gray (because 5 is between 4 and 6.. matplotlib), 1.0 to white
     affordance_map = np.copy(affordance_map)*10
     bounds = [0, 4, 6, 10]
     norm = BoundaryNorm(bounds, cmap.N)
-    fig, axeslist = plt.subplots(ncols=2, nrows=5)
-    for i in range(affordance_map.shape[2]):
-        title = AFFORDANCES[i]
-        axeslist.ravel()[i].imshow(affordance_map[:,:,i], cmap=cmap, norm=norm, interpolation='nearest')
-        axeslist.ravel()[i].set_title(title)
-        axeslist.ravel()[i].set_axis_off()
-    plt.tight_layout()
-    plt.show()
-            
+    if not only_solid_permeable:
+        fig, axeslist = plt.subplots(ncols=2, nrows=5)
+        fig.suptitle(filename)
+        for i in range(affordance_map.shape[2]):
+            title = AFFORDANCES[i]
+            axeslist.ravel()[i].imshow(affordance_map[:,:,i], cmap=cmap, norm=norm, interpolation='nearest')
+            axeslist.ravel()[i].set_title(title)
+            axeslist.ravel()[i].set_axis_off()
+        plt.tight_layout()
+        plt.show()
+    else:
+        fig, axeslist = plt.subplots(ncols=2, nrows=1)
+        fig.suptitle(filename)
+        axeslist.ravel()[0].imshow(affordance_map[:,:,AFFORDANCES.index('solid')], cmap=cmap, norm=norm, interpolation='nearest')
+        axeslist.ravel()[0].set_title('solid')
+        axeslist.ravel()[0].set_axis_off()
+        axeslist.ravel()[1].imshow(affordance_map[:,:,AFFORDANCES.index('permeable')], cmap=cmap, norm=norm, interpolation='nearest')
+        axeslist.ravel()[1].set_title('permeable')
+        axeslist.ravel()[1].set_axis_off()
+        plt.tight_layout()
+        plt.show()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Textrue match detection')
 
-    parser.add_argument('--file', type=str, default="./Super Mario Bros/Processed/mario-8-1.txt")
+    parser.add_argument('--file', type=str, default='')
     parser.add_argument('--json', type=str, default="./AffordanceConversion/smb_affordances.json")
     parser.add_argument('--folder', type=str, default='')
     parser.add_argument('--dest', type=str, default="./AffordanceConversion/SMB/")
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--visualize', action='store_true')
+    parser.add_argument('--visualizeall', action='store_true')
     parser.add_argument('--no-save', action='store_true')
     parser.add_argument('--grid-size', type=int,
                         default=16, help='grid square size')
